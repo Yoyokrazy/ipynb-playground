32aac89b7212f1f221e87687a5e399b772d6a9e9
Michael Lively <milively@microsoft.com>
2024-06-25 10:23:09 -0700

    stuff

---
+++ _code/test.py
@@ -0,0 +1,20 @@
+my_dict = {
+    'key1': 'value1',
+    'key2': 'value2',
+    'key3': 'value3',
+    'key4': 'value4'
+}
+
+class MyClass:
+    def my_function(self):
+        print("Hello, world!")
+        print("bye, world!")
+
+    print("asdf, world!")
+    print("fghj, world!")
+
+# Create an instance of MyClass
+my_instance = MyClass()
+
+# Call the my_function method
+my_instance.my_function()
---
+++ _notebooks/import time.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import time
+
+for meow in range(10):
+    print(meow)
+    # time.sleep(0.1)
