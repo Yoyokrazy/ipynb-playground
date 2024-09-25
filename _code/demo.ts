f074af07c83c1d0059769f0aa8ef11a0974dea8b
Michael Lively <milively@microsoft.com>
2024-09-25 13:28:24 -0700

    asdf

--- _code/demo.ts
+++ _code/demo.ts
@@ -9,28 +9,4 @@ interface PokemonInfo {
 interface a {
   // Define the properties of the 'type' interface here
 class testing {
-}
-const other = 5;
-const time = 5;
-import { StickyScrollWidget } from './codeMyDude';
-import { fib, isPrime } from './fib';
-new StickyScrollWidget();
-async (params:type) => {}
-isPrime()
-async (params:type) => {}
-export function heh(){   fib(20)}
-fib(10);
-// Simulating a Pokémon Pokédex entry
-async function readPokedexEntry() {
-  const pokemonName = 'Pikachu';
-  const filePath = path.join(__dirname, `${pokemonName.toLowerCase()}.json`);
-  try {    const fileContent = await fs.readFile(filePath, 'utf8');
-    const pokemonData: PokemonInfo = JSON.parse(fileContent);
-    console.log(`Pokédex entry for ${pokemonData.name}:`);
-    console.log(`Type: ${pokemonData.type}`);
-    console.log(`Description: ${pokemonData.description}`);
-  } catch (err) {    console.error(`print hello world Error reading ${pokemonName}'s Pokédex entry:`, err);
-  }
-}
-readPokedexEntry();
-const test = new Disposable();
+
