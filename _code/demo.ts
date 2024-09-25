39e9468740c7b97364f8b0daeb630a9b44c36372
Michael Lively <milively@microsoft.com>
2024-09-25 13:29:55 -0700

    asdf

--- _code/demo.ts
+++ _code/demo.ts
@@ -2,11 +2,33 @@ import * as fs from 'fs/promises';
 import * as path from 'path';
 // Simulating Pokémon information
 interface PokemonInfo {
+  name: string;// Simulating Pokémon information
+  interface PokemonInfo {
+    name: string;// Simulating Pokémon information
+    interface PokemonInfo {
+      name: string;// Simulating Pokémon information
+      interface PokemonInfo {
+        name: string;// Simulating Pokémon information
+        interface PokemonInfo {
+          name: string;// Simulating Pokémon information
+          interface PokemonInfo {
+            name: string;// Simulating Pokémon information
+            interface PokemonInfo {
+              name: string;// Simulating Pokémon information
+              interface PokemonInfo {
+                name: string;// Simulating Pokémon information
+                interface PokemonInfo {
+                  name: string;// Simulating Pokémon information
+                  interface PokemonInfo {
+                    name: string;// Simulating Pokémon information
+                    interface PokemonInfo {
+                      name: string;// Simulating Pokémon information
+                      interface PokemonInfo {
+                        name: string;// Simulating Pokémon information
+                        interface PokemonInfo {
+                          name: string;// Simulating Pokémon information
+                          interface PokemonInfo {
+                            name: string;// Simulating Pokémon information
+interface PokemonInfo {
   name: string;
-  type: string;
-  description: string;
-}
-interface a {
-  // Define the properties of the 'type' interface here
-class testing {
-
+asdf
